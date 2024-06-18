a8fccbbcd1ddf15d107ed91788dfc2fc86e4c99a
vivekkumar2909 <rajvivek620065@gmail.com>
2024-06-18 22:40:18 +0530

    ''Update Done"

--- src/constants/images.js
+++ src/constants/images.js
@@ -4,7 +4,6 @@ import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
-// import cpp1 from '../assets/Cpp.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
